dcc875a5775a79d41ff0a139a1ffcb283615dde9
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-15 14:40:28 -0300

    Ej2v2 Hice el tablero de ajedrez

--- Ej2/script.js
+++ Ej2/script.js
@@ -2,6 +2,8 @@ const divTablero = document.querySelector(".tablero");
 const inputFilas = document.querySelector("#filas");
 const inputColumnas = document.querySelector("#columnas");
 
+let color = [0,1];
+
 function crearTablero() {
   const MAX_FIL = parseInt(inputFilas.value);
   const MAX_COL = parseInt(inputColumnas.value);
@@ -10,19 +12,20 @@ function crearTablero() {
 
   for (let fila = 0; fila < MAX_FIL; fila++) {
     tablero += `
-      <div class="row">${agregarCol(MAX_COL)}</div>`;
+      <div class="row">${agregarCol(fila,MAX_COL)}</div>`;
   }
   divTablero.innerHTML = tablero;
+  cambiarColor();
 }
 
-function agregarCol(max_col) {
+function agregarCol(fila,max_col) {
   let columnas = "";
   for (let col = 0; col <max_col; col++) {
-    columnas += `<div class="col casilla"></div>`;
+    let coloresTablero = (fila+col) % 2 === 0 ? 'casilla0' : 'casilla1';
+    columnas += `<div class="col ${coloresTablero}"></div>`;
   }
   return columnas;
 }
-
 
 
 
