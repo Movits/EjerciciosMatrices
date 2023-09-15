f2f14c7f30cd9498fd44b4fc77a291b9488e9551
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-15 02:20:37 -0300

    agregue comentario de como hacer el maquetado rapido en html

--- Ej2/script.js
+++ Ej2/script.js
@@ -2,7 +2,7 @@ setInterval(() => {
     document.querySelectorAll(".casilla").forEach((casilla) => {
         casilla.style.backgroundColor = `rgb(${cambiarColor(255)}, ${cambiarColor(255)}, ${cambiarColor(255)})`;
     });
-}, 1000);
+}, 100);
 
 function cambiarColor(max) {
     return Math.floor(Math.random() * max);
