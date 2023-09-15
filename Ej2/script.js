77fb8065ef7badd2300c09c8fdf7305fadd3fa24
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-15 03:50:29 -0300

    agregue el maquetado para el Ej2v1

--- Ej2/script.js
+++ Ej2/script.js
@@ -1,3 +1,27 @@
+const divTablero = document.querySelector(".tablero");
+
+const MAX_FIL = 5;
+const MAX_COL = 5;
+
+for (let fila = 0; fila < MAX_FIL; fila++) {
+  divTablero.innerHTML += `
+    <div class="row">${agregarCol()}</div>`;
+}
+
+function agregarCol() {
+  let columnas = "";
+  for (let col = 0; col < MAX_COL; col++) {
+    columnas += `<div class="col casilla"></div>`;
+  }
+  return columnas;
+}
+
+
+
+
+
+
+
 /* setInterval(() => {
     document.querySelectorAll(".casilla").forEach((casilla) => {
         casilla.style.backgroundColor = `rgb(${cambiarColor(255)}, ${cambiarColor(255)}, ${cambiarColor(255)})`;
