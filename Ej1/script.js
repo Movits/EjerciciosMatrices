18eb54d93c4a6a2aa8f38a17f118a3dd5a43485a
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-15 01:53:07 -0300

    contadorMax=50

--- Ej1/script.js
+++ Ej1/script.js
@@ -1,12 +1,15 @@
 const matrix = [];
 
 let contador = 1;
+const contadorMax = 50;
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 1; i++) {
   matrix.push([]); // Push una fila para cada iteración
-  for (let j = 0; j < 10; j++) {
-    matrix[i].push(contador); // Push el valor del contador en la fila
-    contador++; // Incrementar el contador en cada iteración
+  for (let j = 0; j < 1; j++) {
+    while (contador <= contadorMax) {
+      matrix[i].push(contador); // Push el valor del contador en la fila
+      contador++; // Incrementar el contador en cada iteración
+    }
   }
   console.log(matrix[i].join(' ')); // Junta los valores de la fila con un espacio
 }
