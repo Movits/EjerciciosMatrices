44f73f719c67f916b061c6034c72edbc09f8ad8f
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-15 02:14:56 -0300

    agregue un set interval para cambiar colores a cada segundo

--- Ej2/script.js
+++ Ej2/script.js
@@ -1,6 +1,8 @@
-document.querySelectorAll(".casilla").forEach((casilla) => {
-    casilla.style.backgroundColor = `rgb(${cambiarColor(255)}, ${cambiarColor(255)}, ${cambiarColor(255)})`;
-});
+setInterval(() => {
+    document.querySelectorAll(".casilla").forEach((casilla) => {
+        casilla.style.backgroundColor = `rgb(${cambiarColor(255)}, ${cambiarColor(255)}, ${cambiarColor(255)})`;
+    });
+}, 1000);
 
 function cambiarColor(max) {
     return Math.floor(Math.random() * max);
