f56595a7f4d7a33a15326aaabc3c8e8d117c5295
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-16 20:50:35 -0300

    agregue el jugador y un eventlistener para las teclas de movimiento

--- Ej3/script.js
+++ Ej3/script.js
@@ -3,18 +3,40 @@ const divTablero = document.querySelector("#tablero");
 const MAX_FILA = 7;
 const MAX_COL = 7;
 
+const POS_INICIAL_COL = 3;
+const POS_INICIAL_FILA = 3;
+
+const ARRIBA = "w";
+const ABAJO = "s";
+const IZQUIERDA = "a";
+const DERECHA = "d";
+
+document.addEventListener("keydown", function(event) {
+    alert(event.key);
+});
+
+/* Genera el Tablero de la pagina */
 function generarMatriz() {
     divTablero.innerHTML = "";    
     for (let fila = 0; fila < MAX_FILA; fila++) {
-        /* matriz.push([])    */ 
         divTablero.innerHTML += `
             <div class="row">
                 ${ agregarColumnas(fila) }
             </div>
         `;
     }
+    agregarJugador(POS_INICIAL_FILA, POS_INICIAL_COL);
 }
 
+
+/* Agrega el jugador en la posicion indicada */
+function agregarJugador(fila, col) {
+    let jugador = document.querySelector(`#casilla-${fila + "-" + col}`);
+    jugador.innerHTML = `<span>J</span>`;
+    jugador.classList.add("jugador");
+}
+
+/* Genera las casillas correspondientes a la fila actual */
 function agregarColumnas(fila) {
     let columnasGeneradasHtml = "";
     for (let columna = 0; columna < MAX_COL; columna++) {
