08d71248da167bc42f38e613921c089fe72ad2dc
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-16 19:18:53 -0300

    termine el maquetado inicial del ej3

--- Ej3/script.js
+++ Ej3/script.js
@@ -1,53 +1,27 @@
 const divTablero = document.querySelector("#tablero");
-const inputFila = document.querySelector("#fila");
-const inputCol = document.querySelector("#col");
 
-/* const MAX_FILA = 3;
-const MAX_COL = 5; */
-
-let cantCasillas = 0;
+const MAX_FILA = 7;
+const MAX_COL = 7;
 
 function generarMatriz() {
-    let maxFila = parseInt(inputFila.value);
-    cantCasillas = 0;
     divTablero.innerHTML = "";    
-    for (let fila = 0; fila < maxFila; fila++) {
+    for (let fila = 0; fila < MAX_FILA; fila++) {
         /* matriz.push([])    */ 
         divTablero.innerHTML += `
             <div class="row">
-                ${ agregarColumnas() }
+                ${ agregarColumnas(fila) }
             </div>
-        `
+        `;
     }
 }
 
-function agregarColumnas() {
-    let maxFila = parseInt(inputCol.value);
+function agregarColumnas(fila) {
     let columnasGeneradasHtml = "";
-    for (let columna = 0; columna < maxFila; columna++) {
-        if (cantCasillas%2 == 0) {
-            columnasGeneradasHtml += `
-                <div id="negra" onclick="detectar(this)" class="col casilla casilla-1">
-                </div>
-            `;
-        } else {
-            columnasGeneradasHtml += `
-                <div id="blanca" onclick="detectar(this)" class="col casilla casilla-0">
-                </div>
-            `;
+    for (let columna = 0; columna < MAX_COL; columna++) {
+        columnasGeneradasHtml += `
+            <div id="casilla-${fila+"-"+columna}" class="col casilla"></div>`;
         }
-        cantCasillas++;
-    }
-    cantCasillas++;
     return columnasGeneradasHtml;
 }
 
-/**
- * detecta la casilla
- * @param {HTMLDivElement} elemento casilla
- */
-function detectar(elemento) {
-    elemento.style.backgroundColor = "red";
-    alert(elemento.id);
-    console.log(elemento.id);
-}
+generarMatriz();
