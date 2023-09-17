d6f4001b497132dca33d06ac2a08eaa42e5d7252
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-17 00:43:49 -0300

    agregue un enemigo que anda en movimientos random y resultado de perdiste si el enemigo alcanza el jugador

--- Ej3/script.js
+++ Ej3/script.js
@@ -11,14 +11,25 @@ const POS_INICIAL_FILA = 3;
 const POS_GANASTE_COL = 3;
 const POS_GANASTE_FILA = 0;
 
+/* Posicion Inicial Enemigo */
+const POS_INICIAL_ENEMIGO_COL = 3;
+const POS_INICIAL_ENEMIGO_FILA = 6;
+
 /* Teclas movimiento */
 const ARRIBA = "w";
 const ABAJO = "s";
 const IZQUIERDA = "a";
 const DERECHA = "d";
 
-let posJugador = [POS_INICIAL_FILA, POS_INICIAL_COL]
-let posCasillaGanaste = [POS_GANASTE_FILA, POS_GANASTE_COL]
+let posJugador = [POS_INICIAL_FILA, POS_INICIAL_COL];
+let posCasillaGanaste = [POS_GANASTE_FILA, POS_GANASTE_COL];
+let posEnemigo = [POS_INICIAL_ENEMIGO_FILA, POS_INICIAL_ENEMIGO_COL];
+
+/* Verifica si la posicion esta dentro del tablero */
+function limiteTablero(fila, col) {
+    return fila >= 0 && fila < MAX_FILA && col >= 0 && col < MAX_COL;
+}
+
 
 /* Evento que se ejecuta cuando se presiona una tecla */
 document.addEventListener("keydown", function(event) {
@@ -27,32 +38,85 @@ document.addEventListener("keydown", function(event) {
     jugador.innerHTML = ``;
     jugador.classList.remove("jugador");
 
+    let nuevaFila = posJugador[0];
+    let nuevaCol = posJugador[1];
+
     switch (event.key) {
         case ARRIBA:
-            posJugador[0]--;
+            nuevaFila--;
             break;
 
         case ABAJO:
-            posJugador[0]++;
+            nuevaFila++;
             break;
 
         case IZQUIERDA:
-            posJugador[1]--;
+            nuevaCol--;
             break;
 
         case DERECHA:
-            posJugador[1]++;
+            nuevaCol++;
             break;
     }
+
+    /* Verifica si el movimiento esta dentro del limite y actualiza posicion */
+    if (limiteTablero(nuevaFila, nuevaCol)) {
+        posJugador[0] = nuevaFila;
+        posJugador[1] = nuevaCol;
+    }
+
     agregarJugador(posJugador[0], posJugador[1]);
 
+    /* Verificar si el jugador gana o pierde el juego */
     if (posJugador[0] == posCasillaGanaste[0] && posJugador[1] == posCasillaGanaste[1]) {
         setTimeout(() => {
             alert("Ganaste");
             location.reload();
         }, 100);
     }
+
+    if (posJugador[0] == posEnemigo[0] && posJugador[1] == posEnemigo[1]) {
+        setTimeout(() => {
+            alert("Perdiste");
+            location.reload();
+        }, 100);
+    }
 });
+
+/* Movimiento del enemigo */
+function movimientoEnemigo() {
+    setInterval(() => {
+        let enemigo = document.querySelector(`#casilla-${posEnemigo[0] + "-" + posEnemigo[1]}`);
+        enemigo.innerHTML = ``;
+        enemigo.classList.remove("enemigo");
+
+        let direccion = Math.floor(Math.random() * 4);
+
+        switch(direccion) {
+            case 0: // Arriba
+                if(limiteTablero(posEnemigo[0] - 1, posEnemigo[1])) {
+                    posEnemigo[0]--;
+                }
+                break;
+            case 1: // Abajo
+                if(limiteTablero(posEnemigo[0] + 1, posEnemigo[1])) {
+                    posEnemigo[0]++;
+                }
+                break;
+            case 2: // Izquierda
+                if(limiteTablero(posEnemigo[0], posEnemigo[1] - 1)) {
+                    posEnemigo[1]--;
+                }
+                break;
+            case 3: // Derecha
+                if(limiteTablero(posEnemigo[0], posEnemigo[1] + 1)) {
+                    posEnemigo[1]++;
+                }
+                break;
+        }
+        agregarEnemigo(posEnemigo[0], posEnemigo[1]);
+    }, 500);
+}
 
 /* Genera el Tablero de la pagina */
 function generarMatriz() {
@@ -66,7 +130,9 @@ function generarMatriz() {
     }
     agregarJugador(POS_INICIAL_FILA, POS_INICIAL_COL);
     posicionCasillaGanaste(POS_GANASTE_FILA, POS_GANASTE_COL)
+    agregarEnemigo(POS_INICIAL_ENEMIGO_FILA, POS_INICIAL_ENEMIGO_COL);
 }
+
 
 
 /* Agrega el jugador en la posicion indicada */
@@ -74,6 +140,13 @@ function agregarJugador(fila, col) {
     let jugador = document.querySelector(`#casilla-${fila + "-" + col}`);
     jugador.innerHTML = `<span>J</span>`;
     jugador.classList.add("jugador");
+}
+
+/* Agregar un enemigo */
+function agregarEnemigo(fila, col) {
+    let enemigo = document.querySelector(`#casilla-${fila + "-" + col}`);
+    enemigo.innerHTML = `<span>E</span>`;
+    enemigo.classList.add("enemigo");
 }
 
 /* Agrega la casilla ganaste en la posicion indicada */
@@ -95,3 +168,4 @@ function agregarColumnas(fila) {
 }
 
 generarMatriz();
+movimientoEnemigo();
