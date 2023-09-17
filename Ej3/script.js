4cb4424b91cbee9c7dd63094b81b83cf7c2252bf
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-16 23:07:43 -0300

    agregue una casilla para ganar el juego

--- Ej3/script.js
+++ Ej3/script.js
@@ -3,15 +3,22 @@ const divTablero = document.querySelector("#tablero");
 const MAX_FILA = 7;
 const MAX_COL = 7;
 
+/* Posicion Inicial Jugador */
 const POS_INICIAL_COL = 3;
 const POS_INICIAL_FILA = 3;
 
+/* Posicion Casilla Ganaste */
+const POS_GANASTE_COL = 3;
+const POS_GANASTE_FILA = 0;
+
+/* Teclas movimiento */
 const ARRIBA = "w";
 const ABAJO = "s";
 const IZQUIERDA = "a";
 const DERECHA = "d";
 
 let posJugador = [POS_INICIAL_FILA, POS_INICIAL_COL]
+let posCasillaGanaste = [POS_GANASTE_FILA, POS_GANASTE_COL]
 
 /* Evento que se ejecuta cuando se presiona una tecla */
 document.addEventListener("keydown", function(event) {
@@ -38,6 +45,13 @@ document.addEventListener("keydown", function(event) {
             break;
     }
     agregarJugador(posJugador[0], posJugador[1]);
+
+    if (posJugador[0] == posCasillaGanaste[0] && posJugador[1] == posCasillaGanaste[1]) {
+        setTimeout(() => {
+            alert("Ganaste");
+            location.reload();
+        }, 100);
+    }
 });
 
 /* Genera el Tablero de la pagina */
@@ -51,6 +65,7 @@ function generarMatriz() {
         `;
     }
     agregarJugador(POS_INICIAL_FILA, POS_INICIAL_COL);
+    posicionCasillaGanaste(POS_GANASTE_FILA, POS_GANASTE_COL)
 }
 
 
@@ -59,6 +74,14 @@ function agregarJugador(fila, col) {
     let jugador = document.querySelector(`#casilla-${fila + "-" + col}`);
     jugador.innerHTML = `<span>J</span>`;
     jugador.classList.add("jugador");
+}
+
+/* Agrega la casilla ganaste en la posicion indicada */
+function posicionCasillaGanaste(fila, col) {
+    let casillaGanaste = document.querySelector(`#casilla-${fila + "-" + col}`);
+    casillaGanaste.innerHTML = `<span>G</span>`;
+    casillaGanaste.classList.add("ganaste");
+    
 }
 
 /* Genera las casillas correspondientes a la fila actual */
